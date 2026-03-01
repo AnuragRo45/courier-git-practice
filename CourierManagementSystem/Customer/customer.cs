452089ff935608d725b05a5b46da47c6b8d886c3
Amit <amit@test.com>
2026-03-01 16:24:41 +0530

    customer type chagnes2

--- CourierManagementSystem/Customer/customer.cs
+++ CourierManagementSystem/Customer/customer.cs
@@ -31,6 +31,6 @@ namespace CourierManagementSystem.Models
         /// </summary>
         public string DevelopersNotes { get; set; } = string.Empty;
         public string BillType { get; set; } = string.Empty;
-        public string CustomerType { get; set; } = "B2B";
+        public string CustomerType { get; set; } = "C2C";
     }
 }   
